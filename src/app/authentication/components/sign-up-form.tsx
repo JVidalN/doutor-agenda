79125f431b8e979ed1ede0d129cf8abcb553efd2
Jean Vidal <j.vidalnunes@gmail.com>
2025-05-27 22:35:30 -0300

    feat: add login

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import router from "next/router";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
